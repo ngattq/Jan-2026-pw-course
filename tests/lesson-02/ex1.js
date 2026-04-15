759d150c8c6651c0dd5c74e404c9e62501e6a39a
Nga Tran <quynhnga610@gmail.com>
2026-04-15 14:59:48 +0700

    feat: add bai tap lesson 2

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,7 @@
+let a = 11;
+let name = "my number";
+let isEven = false;
+if(a%2 == 0){
+ isEven = true;
+}
+console.log(isEven)
---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,7 @@
+let chieucao = 158;
+let cannanglytuong = (chieucao-100)*9/10;
+let cannangtoida = (chieucao - 100);
+let cannangtoithieu = (chieucao-100)*8/10;
+console.log("cân nặng lý tưởng của bạn là:" +cannanglytuong);
+console.log("cân nặng tối đa của bạn là:" +cannangtoida);
+console.log("cân nặng tối thiểu của bạn là:" +cannangtoithieu);
